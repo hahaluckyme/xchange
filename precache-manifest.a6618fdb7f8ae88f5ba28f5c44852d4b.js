d2c55e92c45885a70091a95010eb7008b87999a5
Brim Fox <helicalzero@gmail.com>
2019-02-03 21:13:02 -0800

    Updates

---
+++ precache-manifest.a6618fdb7f8ae88f5ba28f5c44852d4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9911a64aae3b9a314c5a3cf2d738b808",
+    "url": "/xchange/static/media/Aardvark_Cafe.9911a64a.ttf"
+  },
+  {
+    "revision": "6533b05301c925e76b80",
+    "url": "/xchange/static/js/runtime~main.6533b053.js"
+  },
+  {
+    "revision": "e0294ee3257a00606f74",
+    "url": "/xchange/static/js/main.e0294ee3.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/xchange/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "e0294ee3257a00606f74",
+    "url": "/xchange/static/css/main.7a5d391e.chunk.css"
+  },
+  {
+    "revision": "2befc16dd96ccb972d9db547cfd85c97",
+    "url": "/xchange/index.html"
+  }
+];
